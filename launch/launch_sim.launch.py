a2ca7aa8bdc1af36c85b5e49b9dcb40ad5f3f59a
Void <dnbabkov@gmail.com>
2026-04-10 17:51:39 +0300

    Working version for gazebo sim

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,72 +2,132 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
+    package_name = 'ar_project'
+    pkg_share = get_package_share_directory(package_name)
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='ar_project' #<--- CHANGE ME
+    world = LaunchConfiguration('world')
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_share, 'launch', 'rsp.launch.py')
+        ),
+        launch_arguments={
+            'use_sim_time': 'true',
+            'use_ros2_control': 'true'
+        }.items()
     )
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
+    twist_mux_params = os.path.join(pkg_share, 'config', 'twist_mux.yaml')
     twist_mux = Node(
-        package = "twist_mux",
-        executable = "twist_mux",
-        parameters = [twist_mux_params, {'use_sim_time': True}],
-        remappings = [('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
+        package='twist_mux',
+        executable='twist_mux',
+        parameters=[twist_mux_params, {'use_sim_time': True}],
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')],
+        output='screen'
     )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
+            )
+        ),
+        launch_arguments={
+            'gz_args': [' -r ', world]
+        }.items()
+    )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'my_bot'
+        ],
+        output='screen'
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    lidar_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='lidar_bridge',
+        arguments=[
+            '/lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+        ],
+        remappings=[
+            ('/lidar', '/scan'),
+        ],
+        parameters=[{
+            'override_frame_id': 'laser_frame'
+        }],
+        output='screen'
+    )
 
-
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_cont"],
+    sensors_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='sensors_bridge',
+        arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+            '/camera@sensor_msgs/msg/Image[ignition.msgs.Image',
+            '/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+            '/depth_camera@sensor_msgs/msg/Image[ignition.msgs.Image',
+            '/depth_camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+        ],
+        remappings=[
+            ('/camera', '/camera/image_raw'),
+            ('/camera_info', '/camera/camera_info'),
+            ('/depth_camera', '/depth_camera/depth/image_raw'),
+        ],
+        output='screen'
     )
 
     joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_broad"],
+        package='controller_manager',
+        executable='spawner',
+        name='joint_broad_spawner',
+        arguments=['joint_broad'],
+        output='screen'
     )
 
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        name='diff_drive_spawner',
+        arguments=['diff_cont'],
+        output='screen'
+    )
 
-    # Launch them all!
+    delayed_controller_spawners = TimerAction(
+        period=5.0,
+        actions=[
+            joint_broad_spawner,
+            diff_drive_spawner,
+        ]
+    )
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value=os.path.join(pkg_share, 'worlds', 'test_1.world'),
+            description='Path to Gazebo Sim world file'
+        ),
         rsp,
         twist_mux,
-        gazebo,
-        spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner,
+        gz_sim,
+        spawn_robot,
+        lidar_bridge,
+        sensors_bridge,
+        delayed_controller_spawners,
     ])
